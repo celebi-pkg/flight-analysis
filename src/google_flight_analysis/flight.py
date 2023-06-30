6284ecc09d65bef09eba4a8b645154c46f4127a7
Ion Buzdugan <ion.dbuzdugan@gmail.com>
2023-06-29 22:34:17 -0700

    Update flight.py to include comma parsing for CO2 emissions
    Just like for price parsing, CO2 emission entries also need to account for commas when converting them to int

--- src/google_flight_analysis/flight.py
+++ src/google_flight_analysis/flight.py
@@ -124,7 +124,7 @@ class Flight:
 
 		elif arg.endswith('CO2') and self._co2 is None:
 			# co2
-			self._co2 = int(arg.split()[0])
+			self._co2 = int(arg.split()[0].replace(',',''))
 		elif arg.endswith('emissions') and self._emissions is None:
 			# emmision
 			emission_val = arg.split()[0]
